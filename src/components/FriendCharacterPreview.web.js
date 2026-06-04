b6c8ce0ef51d4fb23a7d3a2a06ab262c359b0068
jeon-stat <jeon981118@gmail.com>
2026-06-04 10:52:35 +0900

    Fix friend preview scaling ratio

--- src/components/FriendCharacterPreview.web.js
+++ src/components/FriendCharacterPreview.web.js
@@ -1,16 +1,25 @@
 import { View } from "react-native";
 
 import { CharacterStage } from "./CharacterStage";
-
-const STAGE_BASE_HEIGHT = 500;
+import { STAGE_LAYOUT } from "../scene/stageConfig.js";
 
 export function FriendCharacterPreview({ character, state, size = 112 }) {
   const scale = Math.max(0.22, Math.min(0.34, size / 420));
-  const shellHeight = Math.round(STAGE_BASE_HEIGHT * scale);
+  const shellHeight = Math.round(STAGE_LAYOUT.heroHeight * scale);
+  const stageWidth = Math.round(size / scale);
 
   return (
-    <View style={{ width: size, height: shellHeight }} pointerEvents="none">
-      <CharacterStage character={character} state={state} scale={scale} onInteractionChange={undefined} />
+    <View style={{ width: size, height: shellHeight, overflow: "hidden" }} pointerEvents="none">
+      <View
+        style={{
+          width: stageWidth,
+          height: STAGE_LAYOUT.heroHeight,
+          transform: [{ scale }],
+          transformOrigin: "top center",
+        }}
+      >
+        <CharacterStage character={character} state={state} onInteractionChange={undefined} />
+      </View>
     </View>
   );
 }
